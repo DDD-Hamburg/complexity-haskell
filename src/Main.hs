927b017cfa032dc48dc463220c1eba7489c32144
Virgilio Lino <virgilio.lino@innogames.com>
2017-01-24 00:21:48 +0100

    first commit

---
+++ src/Main.hs
@@ -0,0 +1,46 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveGeneric #-}
+
+module Main where
+
+import Data.Monoid ((<>))
+import Data.Aeson (FromJSON, ToJSON)
+import GHC.Generics
+import Web.Scotty
+import qualified Data.Text.Lazy as TL
+
+data Item = Item { itemId :: String, desc :: String, pricePerUnit :: Float, quantity :: Int} deriving (Show, Generic)
+type DiscountedItems = [String]
+data Cart = Cart {
+  discount :: Float,
+  discountedItemIds :: DiscountedItems,
+  cart :: [Item]
+} deriving (Show, Generic)
+
+instance ToJSON Item
+instance FromJSON Item
+instance ToJSON Cart
+instance FromJSON Cart
+
+calc_total :: [Item] -> Float
+calc_total [item] = pricePerUnit item * fromIntegral(quantity item)
+calc_total (item:items) = (pricePerUnit item * fromIntegral(quantity item)) + calc_total items
+
+main = do
+  putStrLn "Starting Server..."
+  scotty 3000 $ do
+    get "/hello/:name" $ do
+      name <- param "name"
+      text ("hello " <> name <> "!")
+
+    post "/cart" $ do
+      shoppingCart <- jsonData
+      let items = cart shoppingCart
+      let isDiscounted = any (\x -> elem (itemId x) (discountedItemIds shoppingCart)) items
+      let undiscountedTotal = calc_total(items)
+      let discountedTotal = if isDiscounted
+            then undiscountedTotal * discount shoppingCart
+            else undiscountedTotal
+      json (discountedTotal)
+                                                            
+
